9370ebed6675e92ec73fd4908dbc529d7b8ccfc2
Xuffasch <tristan.lucas@gmail.com>
2021-02-16 18:21:19 +0100

    Add photo functions

--- pages/admin/newProduct.js
+++ pages/admin/newProduct.js
@@ -2,20 +2,25 @@ import Head from 'next/head';
 import Link from 'next/link';
 import s from '../../styles/NewProduct.module.css';
 
-import { useState, useEffect } from 'react'; 
+import { useState, useEffect, useCallback } from 'react'; 
 
 import { v4 as uuidv4 } from 'uuid';
 
-export default function NewProduct() {
+const useMemory = () => {
   const [width, setWidth] = useState(500);
   const [height, setHeight] = useState(375);
   const [streaming, setStreaming] = useState(false);
   const [images, setImages] = useState([]);
   const [name, setName] = useState("Nom Produit");
 
+  return { width, setWidth, height, setHeight, streaming, setStreaming, images, setImages, name, setName }
+}
+
+export default function NewProduct() {
+  const { width, setWidth, height, setHeight, streaming, setStreaming, images, setImages, name, setName } = useMemory();
+
   function startup() {
     let video = document.getElementById('video');
-    let startbutton = document.getElementById('startbutton');
 
     navigator.mediaDevices.getUserMedia({
       video: true,
@@ -35,26 +40,23 @@ export default function NewProduct() {
       }
     }, false)
 
-    startbutton.addEventListener('click', ev => {
-      ev.preventDefault();
-      takephoto();
-    }, false);
-
-    clearphoto();
+    // clearphoto();
   }
 
   function clearphoto() {
-    let canvas = document.getElementById('canvas');
-    let context = canvas.getContext('2d');
-    context.fillStyle = '#AAA';
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    // let canvas = document.getElementById('canvas');
+    // let context = canvas.getContext('2d');
+    // context.fillStyle = '#AAA';
+    // context.fillRect(0, 0, canvas.width, canvas.height);
 
-    var data = canvas.toDataURL('image/png');
+    // var data = canvas.toDataURL('image/png');
     // let photo = document.getElementById('photo');
     // photo.setAttribute('src', data);
+    setImages([]);
   }
 
-  function takephoto() {
+  const takeCapture = useCallback( () => {
+    console.log("current name : ", name);
     let video = document.getElementById('video');
     let canvas = document.getElementById('canvas');
     let context = canvas.getContext('2d');
@@ -66,7 +68,23 @@ export default function NewProduct() {
 
       var data = canvas.toDataURL('image/png');
       setImages(prev => [...prev, {id: uuidv4(), data: data, selected: false, name: name }]);
-      console.log('images : ', images);
+    }
+
+  } , [name])
+
+  function takephoto() {
+    console.log("current name : ", name);
+    let video = document.getElementById('video');
+    let canvas = document.getElementById('canvas');
+    let context = canvas.getContext('2d');
+
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+
+      var data = canvas.toDataURL('image/png');
+      setImages(prev => [...prev, {id: uuidv4(), data: data, selected: false, name: name }]);
 
       // let photo = document.getElementById('photo');
       // photo.setAttribute('src', data);
@@ -78,13 +96,27 @@ export default function NewProduct() {
   const selectImages = (e) => {
     console.log('event target : ', e.target);
     let sIndex = images.findIndex(i => i.id == e.target.id)
-    console.log('sIndex image : ', images[sIndex]);
     let selectedImage = { ...images[sIndex], selected: !images[sIndex].selected };
-    console.log('selected : ', selectedImage);
     let newSelected = [...images.slice(0, sIndex), selectedImage, ...images.slice(sIndex+1)];
     console.log("new list : ", newSelected);
     setImages(newSelected);
-  } 
+  }
+  
+  const deleteImage = (e) => {
+    e.stopPropagation();
+    console.log("Will delete : ", e.target);
+  }
+
+  const changeName = (e) => {
+    console.log("New name from target: ", e.target.value);
+    setName(e.target.value);
+    console.log('New name in state: ', name );
+  }
+
+  const confirmPictures = (e) => {
+    e.stopPropagation();
+    console.log("Confirm images with name : ", name);
+  }
 
   useEffect(() => {
     window.addEventListener('load', startup);
@@ -99,27 +131,34 @@ export default function NewProduct() {
 
       <main>
         <h1 className={s.title}>Create a new product</h1>
-        <input className='block w-full p-2 m-2 bg-yellow-400 border-blue-800' value={name} placeholder='Nouveau produit' onChange={(event) => setName(event.target.value)} />
+        <input className={s.newTitle} value={name} placeholder='Nouveau produit' onChange={changeName} />
         <Link href='/'>
           <a className={s.back}>&#60; NJC</a>
         </Link>
-        <div>
-          
+        <div className='flex flex-col items-center'>
           <div className='camera'>
             <video id='video'>Video stream not available</video>
-            <button className={s.startbutton} id='startbutton' >Take photo</button>
+
           </div>
           <canvas id='canvas' className={s.canvas}>
           </canvas>
-          {/* <div className='output'>
-            <img id='photo' alt="The screen capture will appear here" />
-          </div> */}
+          <button className={`block ${s.startbutton}`} id='startbutton' onClick={takeCapture} >Take photo</button>
+          <span>
+            <button className={s.savebutton} onClickCapture={confirmPictures}>
+              Save Selected Pictures
+            </button>
+            <button className={s.clearbutton} onClickCapture={clearphoto}>
+              Clear Photos
+            </button>
+          </span>
+
           <div id='captured'>
-            <ul className="w-full flex flex-wrap">
+            <ul className="w-full grid grid-cols-2 gap-2">
               {images.map(i => {
                 return (
-                  <li className={'w-1/2 ' + (i.selected ? 'bg-yellow-500' : '')} key={i.id} id={i.id} onClick={selectImages} >
-                    <img src={i.data} alt={i.name} className='border-red-400' id={i.id}/>
+                  <li className={'block relative p-2 ' + (i.selected ? 'bg-green-400' : '')} key={i.id} id={i.id} onClick={selectImages} >
+                    <img src={i.data} alt={i.name} className='w-full z-0' id={i.id} />
+                    <button className='absolute top-0 left-0 p-2 bg-red-400 text-white z-1' onClickCapture={deleteImage}>Delete</button>
                   </li>
                 )
               })}
